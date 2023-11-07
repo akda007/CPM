3e4c30202e56a575374e6edb8d5472feb6b1c420
AndreLuisPLuz <andreluispluz@gmail>
2023-11-07 09:42:32 -0300

    function for listing all files under origin directory (full path needed)

--- src/filetracker/tracker.c
+++ src/filetracker/tracker.c
@@ -1,1 +1,27 @@
-#include <tracker.h>
+#include "tracker.h"
+
+void list_files(char* origin)
+{
+    DIR *d;
+    dirent *dir;
+
+    d = opendir(origin);
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    assert(argc > 1);
+
+    list_files(argv[1]);
+
+    return 0;
+}
--- src/filetracker/tracker.h
+++ src/filetracker/tracker.h
@@ -1,7 +1,14 @@
 #ifndef TRACKER_H
 #define TRACKER_H
 
-    #include <dirent.h>
     #include <stdio.h>
+    #include <string.h>
+    #include <stdbool.h>
+    #include <assert.h>
+    #include <dirent.h>
+
+    typedef struct dirent dirent;
+
+    void list_files(char* origin);
 
 #endif
