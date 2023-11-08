eb857f686617c8fa17794b14183fcda8c1fa4280
AndreLuisPLuz <andreluispluz@gmail.com>
2023-11-08 09:36:46 -0300

    Added linked_list module for use in MEMBERS

---
+++ src/filetracker/linked_list/linked_list.h
@@ -0,0 +1,34 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+    #include <stdlib.h>
+    #include <stdbool.h>
+    #include <assert.h>
+
+    typedef void (*callback_print) (void *data);
+    typedef bool (*callback_compare) (void *data1, void *data2);
+
+    struct Node
+    {
+        void *data;
+        struct Node *next;
+        callback_print cprint;
+        callback_compare ccompare;
+    };
+
+    typedef struct
+    {
+        struct Node *head;
+        struct Node *tail;
+        unsigned length;
+    } LinkedList;
+
+    struct Node * init_node(void *data, callback_print cprint, callback_compare ccompare);
+    void free_node(struct Node *node);
+
+    LinkedList * init_list(struct Node *node);
+    void append_list(LinkedList *list, struct Node *node);
+    void * fetch_index_list(LinkedList *list, unsigned index);
+    void free_list(LinkedList *list);
+
+#endif
